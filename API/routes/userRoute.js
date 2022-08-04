dc8e771fe42efd3c025eb9f5f18ededa7a1ff8d2
kipperliveth <godonamadi687@gmail.com>
2022-08-04 11:31:53 +0100

    Fifth commit

--- API/routes/userRoute.js
+++ API/routes/userRoute.js
@@ -4,7 +4,7 @@ const userRoute = require('../controllers/userController');
 route.post('/api/v1/createuser', userRoute.createUser)
 route.get('/api/v1/getusers',  userRoute.getAllUser)
 route.get('/api/v1/getuser/:id',  userRoute.getSingleUser)
-route.patch('/api/v1updateuser/:id',  userRoute.updateUserById)
-route.delete('/api/v1deleteuser/:id',  userRoute.deleteUser)
+route.patch('/api/v1/updateuser/:id',  userRoute.updateUserById)
+route.delete('/api/v1/deleteuser/:id',  userRoute.deleteUser)
 
 module.exports = route
